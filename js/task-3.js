475e7b2049858ad6e56d1c73cf8526243c91832f
Pivtorak Artem <mulbrry.digital@gmail.com>
2024-12-08 18:37:45 +0200

    hm-2

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,16 @@
-const getElementWidth = (content, padding, border) => {
+function checkForSpam(message) {
 
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding) * 2;
-    const borderWidth = parseFloat(border) * 2;
-
-    const totalWidth = contentWidth + paddingWidth + borderWidth;
-
-    return totalWidth;
-};
+  const lowerCaseMessage = message.toLowerCase();
 
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+  return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
+}
+
+
+console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("JavaScript weekly newsletter"));
+console.log(checkForSpam("Get best sale offers now!"));
+console.log(checkForSpam("Amazing SalE, only tonight!"));
+console.log(checkForSpam("Trust me, this is not a spam message"));
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+console.log(checkForSpam("[SPAM] How to earn fast money?"));
